89f31a88e5afcd4ac992151647f1c2311b14bc7f
vkadambi <vykadambi@yahoo.com>
2019-08-15 21:18:30 -0700

    AHHH

--- Versions/main_v2.py
+++ Versions/main_v2.py
@@ -1,27 +1,74 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 import numpy as np
-import matplotlib.pyplot as plt #Add this
-import seaborn as sns #Add this
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-np.random.seed(0)                                          
+# Date            Programmers                         Descriptions of Change
+# ====         ================                       ======================
+# 08/15/19        Vai                                 Original code
+# 08/15/19.       Vai                                 Main includes function call/plots
+
+np.random.seed(0)
 from ugm_diffusion_v2 import stone,stoneUGM,stoneEta,stoneEtaUGM,ratcliff,ratcliffUGM
-resp, rt, data = stone(0.5,1,1,0,1,0.1,100,1000) #Add outputs
-# stone(0.5,1,1,0,1,0.1,100,1000)
 
-plt.figure()
-result=pd.Series(data) #what exactly is Series in Panda
-result.plot.hist(grid=True, bins=1, rwidth=0.9,color='#607c8e')
-plt.title('Stone Data')
-plt.xlabel('Counts')
-plt.ylabel('Reaction Times')
-plt.grid(axis='y', alpha=0.75)
+#Stone function call
+stone(0.5,1,1,0,1,0.1,100,1000)
+#Stone plot
+plt.figure(1)
+sns.distplot(data, hist=True, kde=False,bins=int(180/5), color = 'blue',hist_kws={'edgecolor':'black'})
+plt.title('Histogram of Stone Data')
+plt.xlabel('Reaction Times')
+plt.ylabel('Frequency')
 plt.savefig("stone.png")
 
-resp, rt, data = stoneUGM(0.5,1,1,0,1,1,1,0.1,100,1000) #Add outputs
+#StoneUGM function call
+stoneUGM(0.5,1,1,0,1,1,1,0.1,100,1000)
+#StoneUGM plot
+plt.figure(2)
+sns.distplot(data, hist=True, kde=False,bins=int(180/5), color = 'blue',hist_kws={'edgecolor':'black'})
+plt.title('Histogram of StoneUGM Data')
+plt.xlabel('Reaction Times')
+plt.ylabel('Frequency')
+plt.savefig("stoneUGM.png")
 
-plt.figure()
-
+#StoneEta function call
 stoneEta(0.5,1,1,1,0,1,0.1,100,1000)
+#StoneEta plot
+plt.figure(3)
+sns.distplot(data, hist=True, kde=False,bins=int(180/5), color = 'blue',hist_kws={'edgecolor':'black'})
+plt.title('Histogram of StoneEta Data')
+plt.xlabel('Reaction Times')
+plt.ylabel('Frequency')
+plt.savefig("stoneEta.png")
+
+#StoneEtaUGM function call
 stoneEtaUGM(0.5,1,1,1,0,1,1,1,0.1,100,1000)
+#StoneEtaUGM plot
+plt.figure(4)
+sns.distplot(data, hist=True, kde=False,bins=int(180/5), color = 'blue',hist_kws={'edgecolor':'black'})
+plt.title('Histogram of StoneEtaUGM Data')
+plt.xlabel('Reaction Times')
+plt.ylabel('Frequency')
+plt.savefig("stoneEtaUGM.png")
+
+#RatCliff function call
+#resp, rt, data = ratcliff(0,1,1,1,0,1,1,0.1,100,1000)
 ratcliff(0,1,1,1,0,1,1,0.1,100,1000)
+#RatCliff plot
+plt.figure(5)
+sns.distplot(data, hist=True, kde=False,bins=int(180/5), color = 'blue',hist_kws={'edgecolor':'black'})
+plt.title('Histogram of RatCliff Data')
+plt.xlabel('Reaction Times')
+plt.ylabel('Frequency')
+plt.savefig("ratcliff.png")
+
+#RatCliffUGM function call
 ratcliffUGM(0,1,1,1,0,1,1,1,1,0.1,100,1000)
+#RatCliffUGM plot
+plt.figure(6)
+sns.distplot(result, hist=True, kde=False,bins=int(180/5), color = 'blue',hist_kws={'edgecolor':'black'})
+plt.title('Histogram of RatCliffUGM Data')
+plt.xlabel('Reaction Times')
+plt.ylabel('Frequency')
+plt.savefig("ratcliffUGM.png")
+
--- Versions/ugm_diffusion_v2.py
+++ Versions/ugm_diffusion_v2.py
@@ -1,30 +1,65 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import math
 import random
 
 # Date            Programmers                         Descriptions of Change
 # ====         ================                       ======================
-# 08/15/19        Vai                              Original code
+# 08/15/19        Vai                                 Original code
 
 def stone (z,v,aU,aL,s,h,n,maxiter) :
     N = int(n)
     Maxiter = int(maxiter)
     rhs = (math.sqrt(h))*s
-    resp=[]
-    rt=[]
-    #evidence = np.empty((maxiter,n))
+    resp = []
+    rt = []
     nDotsVector=np.ones(Maxiter)
     for i in range(N):
         x=z
         iter=0
-        # resp.append(np.nan) #Delete this
-        while (iter<=Maxiter): #Note difference here
+        while (iter<=Maxiter):
             nDots = nDotsVector[iter]
             iter=iter+1
             x=x+h*(v*nDots)+rhs*np.random.normal()
-            # evidence[iter-1,i] = x
+            if (x>=aU):
+                resp.append(float(1.0))
+                resp = np.array(resp)
+                break
+            if (x<=aL):
+                resp.append(float(-1.0))
+                resp = np.array(resp)
+                break
+            if (iter==Maxiter):
+                resp = np.array(resp)
+                resp.append(np.nan)
+                break
+        number=((float(iter))*h)-(h/(float(2.0)))
+        rt.append(number)
+    data = []
+    for i in range(N):
+        temp=resp[i]*rt[i]
+        data.append(temp)
+    print (data)
+    return resp, rt, data #Return all three to test
+def stoneUGM (z,v,aU,aL,timecons,usign,s,h,n,maxiter) :
+    N = int(n)
+    Maxiter = int(maxiter)
+    rhs = (math.sqrt(h))*s
+    resp = []
+    rt = []
+    nDotsVector=np.ones(Maxiter)
+    # weight for expotentionally-weighted moving average
+    # alpha = (h)/(h+timecons)
+    alpha = timecons/(timecons+h)
+    for i in range(N):
+        x=z
+        iter=0
+        while (iter<=Maxiter):
+            nDots = nDotsVector[iter]
+            iter=iter+1
+            #filtered signal from previous step + input from current step
+            x=(alpha*x)+(1-alpha)*(h*(v*nDots))+rhs*np.random.normal()
+            # multiply linear urgency signal, urgency determines size of urgency signal
+            xu =x*iter*(usign) #urgency is multiplicative
             if (x>=aU):
                 resp.append(float(1.0)) #This switch is convention
                 break
@@ -36,105 +71,50 @@ def stone (z,v,aU,aL,s,h,n,maxiter) :
                 break
         number=((float(iter))*h)-(h/(float(2.0)))
         rt.append(number)
-    data=[]
+    data = []
     for i in range(N):
         temp=resp[i]*rt[i]
         data.append(temp)
-    #Get rid of this
-    # result=pd.Series(data) #what exactly is Series in Panda
-    # result.plot.hist(grid=True, bins=1, rwidth=0.9,color='#607c8e')
-    # plt.title('Stone Data')
-    # plt.xlabel('Counts')
-    # plt.ylabel('Reaction Times')
-    # plt.grid(axis='y', alpha=0.75)
-    # plt.savefig("stone.png")
     print (data)
     return resp, rt, data #Return all three to test
-def stoneUGM (z,v,aU,aL,timecons,usign,s,h,n,maxiter) :
-    N = int(n)
-    Maxiter = int(maxiter)
-    rhs = (math.sqrt(h))*s
-    resp=[]
-    rt=[]
-    nDotsVector=np.ones(Maxiter)
-    # weight for expotentionally-weighted moving average
-    # alpha = (h)/(h+timecons)
-    alpha = timecons/(timecons+h)
-    for i in range(N):
-        x=z
-        iter=0
-        resp.append(np.nan)
-        while (iter<Maxiter):
-            nDots = nDotsVector[iter]
-            iter=iter+1
-            #filtered signal from previous step + input from current step
-            x=(alpha*x)+(1-alpha)*(h*(v*nDots))+rhs*np.random.normal()
-            # multiply linear urgency signal, urgency determines size of urgency signal
-            xu =x*iter*(usign) #urgency is multiplicative
-            if (x>=aU):
-                resp.append(float(-1.0))
-                break
-            if (x<=aL):
-                resp.append(float(1.0))
-                break
-        number=((float(iter))*h)-(h/(float(2.0)))
-        rt.append(number)
-    data=[]
-    for i in range(N):
-        temp=resp[i]*rt[i]
-        data.append(temp)
-    result=pd.Series(data) #what exactly is Series in Panda
-    result.plot.hist(grid=True, bins=1, rwidth=0.9,color='#607c8e')
-    plt.title('Stone Data')
-    plt.xlabel('Counts')
-    plt.ylabel('Reaction Times')
-    plt.grid(axis='y', alpha=0.75)
-    plt.savefig("stoneUGM.png")
-    print (data)
-    return data
 def stoneEta (z,v,eta,aU,aL,s,h,n,maxiter) :
     N = int(n)
     Maxiter = int(maxiter)
     rhs = (math.sqrt(h))*s
-    resp=[]
-    rt=[]
+    resp = []
+    rt = []
     nDotsVector=np.ones(Maxiter)
     for i in range(N):
         samplev=v+eta*np.random.normal()
         x=z
         iter=0
-        resp.append(np.nan)
-        while (iter<Maxiter):
+        while (iter<=Maxiter):
             nDots = nDotsVector[iter]
             iter=iter+1
             x=x+h*(samplev*nDots)+rhs*np.random.normal()
             if (x>=aU):
-                resp.append(float(-1.0))
+                resp.append(float(1.0)) #This switch is convention
                 break
             if (x<=aL):
-                resp.append(float(1.0))
+                resp.append(float(-1.0)) #This switch is convention
+                break
+            if (iter==Maxiter): #New if statement
+                resp.append(np.nan)
                 break
         number=((float(iter))*h)-(h/(float(2.0)))
         rt.append(number)
-    data=[]
+    data = []
     for i in range(N):
         temp=resp[i]*rt[i]
         data.append(temp)
-    result=pd.Series(data) #what exactly is Series in Panda
-    result.plot.hist(grid=True, bins=1, rwidth=0.9,color='#607c8e')
-    plt.title('Stone Data')
-    plt.xlabel('Counts')
-    plt.ylabel('Reaction Times')
-    plt.grid(axis='y', alpha=0.75)
-    plt.savefig("stoneEta.png")
     print (data)
-    return data
+    return resp, rt, data #Return all three to test
 def stoneEtaUGM (z,v,eta,aU,aL,timecons,usign,s,h,n,maxiter) :
     N = int(n)
     Maxiter = int(maxiter)
     rhs = (math.sqrt(h))*s
-    resp=[]
-    rt=[]
+    resp = []
+    rt = []
     nDotsVector=np.ones(Maxiter)
     #weight for exponentially-weighted moving average
     #alpha=(*h)/((*h)+(*imecons));
@@ -144,104 +124,90 @@ def stoneEtaUGM (z,v,eta,aU,aL,timecons,usign,s,h,n,maxiter) :
         x=z
         iter=0
         resp.append(np.nan)
-        while (iter<Maxiter):
+        while (iter<=Maxiter):
             nDots = nDotsVector[iter]
             iter=iter+1
             x = alpha*x+(1-alpha)*(h*(samplev*nDots))+rhs*np.random.normal()
             xu=x*iter*usign
             if (x>=aU):
-                resp.append(float(-1.0))
+                resp.append(float(1.0)) #This switch is convention
                 break
             if (x<=aL):
-                resp.append(float(1.0))
+                resp.append(float(-1.0)) #This switch is convention
+                break
+            if (iter==Maxiter): #New if statement
+                resp.append(np.nan)
                 break
         number=((float(iter))*h)-(h/(float(2.0)))
         rt.append(number)
-    data=[]
+    data = []
     for i in range(N):
         temp=resp[i]*rt[i]
         data.append(temp)
-    result=pd.Series(data) #what exactly is Series in Panda
-    result.plot.hist(grid=True, bins=1, rwidth=0.9,color='#607c8e')
-    plt.title('Stone Data')
-    plt.xlabel('Counts')
-    plt.ylabel('Reaction Times')
-    plt.grid(axis='y', alpha=0.75)
-    plt.savefig("ratcliff.png")
     print (data)
-    return data
+    return resp, rt, data #Return all three to test
 def ratcliff (zmin,zmax,v,aU,aL,eta,s,h,n,maxiter) :
     N = int(n)
     Maxiter = int(maxiter)
     rhs = (math.sqrt(h))*s
-    resp=[]
-    rt=[]
+    resp = []
+    rt = []
     nDotsVector=np.ones(Maxiter)
     for i in range(N):
         samplev=v+eta*np.random.normal()
         x=zmin+(zmax-zmin)*np.random.uniform()
         iter=0
-        resp.append(np.nan)
-        while (iter<Maxiter):
+        while (iter<=Maxiter):
             nDots = nDotsVector[iter]
             iter=iter+1
             x = x+h*(samplev*nDots)+rhs*np.random.normal()
             if (x>=aU):
-                resp.append(float(-1.0))
+                resp.append(float(1.0)) #This switch is convention
                 break
             if (x<=aL):
-                resp.append(float(1.0))
+                resp.append(float(-1.0)) #This switch is convention
+                break
+            if (iter==Maxiter): #New if statement
+                resp.append(np.nan)
                 break
         number=((float(iter))*h)-(h/(float(2.0)))
         rt.append(number)
-    data=[]
+    data = []
     for i in range(N):
         temp=resp[i]*rt[i]
         data.append(temp)
-    result=pd.Series(data) #what exactly is Series in Panda
-    result.plot.hist(grid=True, bins=1, rwidth=0.9,color='#607c8e')
-    plt.title('Stone Data')
-    plt.xlabel('Counts')
-    plt.ylabel('Reaction Times')
-    plt.grid(axis='y', alpha=0.75)
-    plt.savefig("ratcliff.png")
     print (data)
-    return data
+    return resp, rt, data #Return all three to test
 def ratcliffUGM (zmin,zmax,v,aU,aL,eta,timecons,usign,s,h,n,maxiter) :
     N = int(n)
     Maxiter = int(maxiter)
     rhs = (math.sqrt(h))*s
-    resp=[]
-    rt=[]
+    resp = []
+    rt = []
     nDotsVector=np.ones(Maxiter)
     for i in range(N):
         samplev=v+eta*np.random.normal()
         x=zmin+(zmax-zmin)*np.random.uniform()
         iter=0
-        resp.append(np.nan)
-        while (iter<Maxiter):
+        while (iter<=Maxiter):
             nDots = nDotsVector[iter]
             iter=iter+1
             x = x+h*(samplev*nDots)+rhs*np.random.normal()
             xu=x*iter*usign
             if (x>=aU):
-                resp.append(float(-1.0))
+                resp.append(float(1.0)) #This switch is convention
                 break
             if (x<=aL):
-                resp.append(float(1.0))
+                resp.append(float(-1.0)) #This switch is convention
+                break
+            if (iter==Maxiter): #New if statement
+                resp.append(np.nan)
                 break
         number=((float(iter))*h)-(h/(float(2.0)))
         rt.append(number)
-    data=[]
+    data = []
     for i in range(N):
         temp=resp[i]*rt[i]
         data.append(temp)
-    result=pd.Series(data) #what exactly is Series in Panda
-    result.plot.hist(grid=True, bins=1, rwidth=0.9,color='#607c8e')
-    plt.title('Stone Data')
-    plt.xlabel('Counts')
-    plt.ylabel('Reaction Times')
-    plt.grid(axis='y', alpha=0.75)
-    plt.savefig("ratcliffUGM.png")
     print (data)
-    return data
+    return resp, rt, data #Return all three to test
