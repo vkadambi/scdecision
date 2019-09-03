ed25e3ef485bf9e006233ef65cf229c593fdd617
vkadambi <vykadambi@yahoo.com>
2019-09-03 00:00:20 -0700

    Making check nans file

--- check_nans.py
+++ check_nans.py
@@ -1,54 +1,72 @@
 #!/usr/bin/python
 import numpy as np
-
+#Questions
+# how many decimals should we round to
 np.random.seed(0)
 from ugm_diffusion import stone,stoneUGM,stoneEta,stoneEtaUGM,ratcliff,ratcliffUGM
 
-#parameter definitions - that can change
-z = 0.5 #bias
-v = -1 #drift rate between -.9 to .9 or -9 to 9 depending on s
-aU = 0.7 #lower limit is always 0 (0.7 to 1.5)
-aL = 0 #lower limit is always 0
-s = 0.1 #diffusion coefficient 0.1 or 1
+def random_parameter() :
+    #upper boundary
+    aU = round(np.random.uniform(0.7,1.5),2)
+    #bias
+    z = np.random.uniform(0,aU)
+    beta = round((z/aU),2)
+    #diffusion coefficient
+    tester_s = round(np.random.uniform(0,1),0)
+    if (tester_s == 0.0):
+        s = 0.1
+    elif (tester_s == 1.0):
+        s = 1.0
+    #drift rate
+    if (s == 0.1):
+        v = round(np.random.uniform(-0.9,0.9),2)
+    elif (s == 1.0):
+        v = round(np.random.uniform(-9,9),2)
+    usign = 1
+    eta = 1
+    return beta,aU,s,v,usign,eta
 
-#parameter definitions - that cannot change
+#generate all the random variables
+beta,aU,s,v,usign,eta = random_parameter()
 h = 0.001 #time resolution
 n = 100 #number of trials
 maxiter = 1000 #number of steps
-timecons = 1
-usign = 1
-eta = 1
+timecons = 1000
 zmin = 0
 zmax = 1
 
-data_stone = stone(z,v,aU,aL,s,h,n,maxiter)
+#checking all the functions
+data_stone = stone(beta,v,aU,s,h,n,maxiter)
 data_stone_check = np.isnan(data_stone)
 for i in range(len(data_stone_check)):
     if i is False:
-        print ("stone NANS")
-data_stoneUGM = stoneUGM(z,v,aU,aL,timecons,usign,s,h,n,maxiter)
+        print ("stone NANS :",beta,v,aU,s)
+data_stoneUGM = stoneUGM (beta,v,aU,timecons,usign,s,h,n,maxiter)
 data_stoneUGM_check = np.isnan(data_stoneUGM)
 for i in range(len(data_stoneUGM_check)):
     if i is False:
-        print ("stoneUGM NANS")
-data_stoneEta = stoneEta(z,v,eta,aU,aL,s,h,n,maxiter)
+        print ("stoneUGM NANS:",beta,v,aU,timecons,usign,s)
+data_stoneEta = stoneEta (beta,v,eta,aU,s,h,n,maxiter)
 data_stoneEta_check = np.isnan(data_stoneEta)
 for i in range(len(data_stoneEta_check)):
     if i is False:
-        print ("stoneEta NANS")
-data_EtaUGM = stoneEtaUGM(z,v,eta,aU,aL,timecons,usign,s,h,n,maxiter)
+        print ("stoneEta NANS:",beta,v,eta,aU,s)
+data_EtaUGM = stoneEtaUGM (beta,v,eta,aU,timecons,usign,s,h,n,maxiter)
 data_EtaUGM_check = np.isnan(data_EtaUGM)
 for i in range(len(data_EtaUGM_check)):
     if i is False:
-        print ("EtaUGM NANS")
-data_ratcliff = ratcliff(zmin,zmax,v,aU,aL,eta,s,h,n,maxiter)
+        print ("EtaUGM NANS:",beta,v,eta,aU,timecons,usign,s)
+data_ratcliff = ratcliff (zmin,zmax,v,aU,eta,s,h,n,maxiter)
 data_ratcliff_check = np.isnan(data_ratcliff)
 for i in range(len(data_ratcliff_check)):
     if i is False:
-        print ("ratcliff NANS")
-data_ratcliffUGM = ratcliffUGM(zmin,zmax,v,aU,aL,eta,timecons,usign,s,h,n,maxiter)
+        print ("ratcliff NANS:",v,aU,eta,s)
+data_ratcliffUGM = ratcliffUGM (zmin,zmax,v,aU,eta,timecons,usign,s,h,n,maxiter)
 data_ratcliffUGM_check = np.isnan(data_ratcliffUGM)
 for i in range(len(data_ratcliffUGM_check)):
     if i is False:
-        print ("ratcliffUGM NANS")
+        print ("ratcliffUGM NANS:",v,aU,eta,timecons,usign,s)
+
+#worked out well, print the variables
+print("All good!",beta,",",v,",",eta,",",aU,",",timecons,",",usign,",",s)
 
