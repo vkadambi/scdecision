42178c0e0271955c95c9edc2f8d030b14c800209
vkadambi <vykadambi@yahoo.com>
2019-09-27 01:16:48 -0700

    got rid of rounding, set aU dependent on s, added percentage of nans

--- check_nans.py
+++ check_nans.py
@@ -4,9 +4,6 @@ import numpy as np
 from ugm_diffusion import stone,stoneUGM,stoneEta,stoneEtaUGM,ratcliff,ratcliffUGM
 
 def random_parameter() :
-    #bias
-    z = np.random.uniform(0,aU)
-    beta = round((z/aU),2)
     #diffusion coefficient
     tester_s = round(np.random.uniform(0,1),0)
     if (tester_s == 0.0):
@@ -23,6 +20,9 @@ def random_parameter() :
         aU = np.random.uniform(0.07,0.15)
     elif (s == 1.0):
         aU = np.random.uniform(0.7,1.5)
+    #bias
+    z = np.random.uniform(0,aU)
+    beta = round((z/aU),2)
     #trial variability of the drift rate
     eta = round(np.random.uniform(0,2),2)
     #urgency signal
@@ -38,7 +38,7 @@ def random_parameter() :
     # bias min and max
     zmin = 0
     zmax = 1
-
+    
     #return the random parameter
     return beta,zmin,zmax,v,eta,aU,timecons,usign,s,h,n,maxiter
 
@@ -48,40 +48,69 @@ for i in range(100) :
     #checking all the functions
     data_stone = stone(beta,v,aU,s,h,n,maxiter)
     data_stone_check = np.isnan(data_stone)
+    count = 0
     for i in range(len(data_stone_check)):
         if (data_stone_check[i]==True):
-            print ("stone NANS :",beta,v,aU,s,h,n,maxiter)
-            break
+            count+=1
+    if (count > 0):
+        print ("stone NANS:",beta,v,aU,timecons,usign,s,h,n,maxiter)
+        percent = count/(len(data_stone_check))
+        print ("Percentage of NaNs in Stone",percent)
+        break
     data_stoneUGM = stoneUGM (beta,v,aU,timecons,usign,s,h,n,maxiter)
     data_stoneUGM_check = np.isnan(data_stoneUGM)
+    count = 0
     for i in range(len(data_stoneUGM_check)):
         if (data_stoneUGM_check[i]==True):
-            print ("stoneUGM NANS:",beta,v,aU,timecons,usign,s,h,n,maxiter)
-            break
+            count+=1
+    if (count > 0):
+        print ("stoneUGM NANS:",beta,v,aU,timecons,usign,s,h,n,maxiter)
+        percent = count/(len(data_stoneUGM_check))
+        print ("Percentage of NaNs in StoneUGM",percent)
+        break
     data_stoneEta = stoneEta (beta,v,eta,aU,s,h,n,maxiter)
     data_stoneEta_check = np.isnan(data_stoneEta)
+    count = 0
     for i in range(len(data_stoneEta_check)):
         if (data_stoneEta_check[i]==True):
-            print ("stoneEta NANS:",beta,v,eta,aU,s,h,n,maxiter)
-            break
+            count+=1
+    if (count > 0):
+        print ("stoneEta NANS:",beta,v,eta,aU,s,h,n,maxiter)
+        percent = count/(len(data_stoneEta_check))
+        print ("Percentage of NaNs in StoneEta",percent)
+        break
     data_EtaUGM = stoneEtaUGM (beta,v,eta,aU,timecons,usign,s,h,n,maxiter)
     data_EtaUGM_check = np.isnan(data_EtaUGM)
+    count = 0
     for i in range(len(data_EtaUGM_check)):
         if (data_EtaUGM_check[i]==True):
-            print ("EtaUGM NANS:",beta,v,eta,aU,timecons,usign,s,h,n,maxiter)
-            break
+            count+=1
+    if (count > 0):
+        print ("EtaUgm NANS:",beta,v,eta,aU,timecons,usign,s,h,n,maxiter)
+        percent = count/(len(data_EtaUGM_check))
+        print ("Percentage of NaNs in EtaUgm",percent)
+        break
     data_ratcliff = ratcliff (zmin,zmax,v,aU,eta,s,h,n,maxiter)
     data_ratcliff_check = np.isnan(data_ratcliff)
+    count = 0
     for i in range(len(data_ratcliff_check)):
         if (data_ratcliff_check[i]==True):
-            print ("ratcliff NANS:",zmin,zmax,v,aU,eta,s,h,n,maxiter)
-            break
+            count+=1
+    if (count > 0):
+        print ("ratcliff NANS:",zmin,zmax,v,aU,eta,s,h,n,maxiter)
+        percent = count/(len(data_ratcliff_check))
+        print ("Percentage of NaNs in ratcliff",percent)
+        break
     data_ratcliffUGM = ratcliffUGM (zmin,zmax,v,aU,eta,timecons,usign,s,h,n,maxiter)
     data_ratcliffUGM_check = np.isnan(data_ratcliffUGM)
+    count = 0
     for i in range(len(data_ratcliffUGM_check)):
         if (data_ratcliffUGM_check[i]==True):
-            print ("ratcliffUGM NANS:",zmin,zmax,v,aU,eta,timecons,usign,s,h,n,maxiter)
-            break
+            count+=1
+    if (count >0):
+        print ("ratcliffUGM NANS:",zmin,zmax,v,aU,eta,timecons,usign,s,h,n,maxiter)
+        percent = count/(len(data_ratcliffUGM_check))
+        break
 
 #worked out well, print the variables
 print("All good!")
