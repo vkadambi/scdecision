ad5c40b1f0aa333c4491ca0becdcfc297cb420a4
Vyshu Kadambi <vyshukadambi@Vyshus-MacBook-Pro.local>
2020-03-23 13:04:01 -0700

    fitting file fixed

--- HDDM/fitting_file.py
+++ HDDM/fitting_file.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import hddm
+import numpy as np
+import math
+import random
 import seaborn as sns
 import csv
 import hddm
@@ -10,6 +16,52 @@ from recovery import recovery
 
 #first define the stone function
 def stone (beta,v,aU,s,h,n,maxiter) :
+    N = int(n)
+    Maxiter = int(maxiter)
+    rhs = (math.sqrt(h))*s
+    resp = []
+    rt = []
+    data = []
+    nDotsVector=np.ones(Maxiter)
+    for i in range(N):
+        x=beta*aU
+        iter=0
+        while (iter<=Maxiter):
+            nDots = nDotsVector[iter]
+            iter=iter+1
+            x=x+h*(v*nDots)+rhs*np.random.normal()
+            if (x>=aU):
+                resp.append(float(1.0))
+                break
+            if (x<=0):
+                resp.append(float(-1.0))
+                break
+            if (iter==Maxiter):
+                resp.append(np.nan)
+                break
+        number=((float(iter))*h)-(h/(float(2.0)))
+        rt.append(number)
+    for i in range(N):
+        temp=resp[i]*rt[i]
+        data.append(temp)
+    return data
+    
+def random_parameter() :
+    #diffusion coefficient
+    s = 1.0
+    #drift rate
+    v = np.random.uniform(-9,9)
+    #upper boundary
+    aU = np.random.uniform(0.4,2.0)
+    #bias
+    beta = 0.5
+    #time resolution
+    h = 0.001
+    #number of trials
+    n = 100
+    #number of steps
+    maxiter = 1000
+    #return random parameters
     return beta,v,aU,s,h,n,maxiter
 
 # generate the data we want using a set of defined parameters, take out the Nans
@@ -25,6 +77,29 @@ for i in range(30):
     data = data[~np.isnan(data)]
     rt = np.concatenate((rt,data),axis=None)
     #now add a subject number column
+    for j in range(len(data)):
+        subj_idx.append(i)
+#take out all the Nans
+rt = rt[~np.isnan(rt)]
+# create an array for the response (1 for positive and 0 for negative)
+response = []
+for i in rt:
+    if (i > 0 ):
+        response.append(1)
+    elif (i < 0):
+        response.append(0)
+response = np.array(response)
+# since we have the response data, we use the absolute value for the rts
+for i in range(len(rt)):
+    rt[i] = abs(rt[i])
+
+#put the array into a csv file
+f = open('data.csv', 'wb')
+out = csv.writer(f, delimiter=',')
+out.writerow(["subj_idx","rt","response"])
+for i in range(len(rt)):
+    list = [subj_idx[i],rt[i],response[i]]
+    out.writerows([list])
 f.close()
 
 #plot for fun
@@ -32,6 +107,39 @@ plt.figure(5)
 sns.distplot(data[~np.isnan(data)], hist=True, kde=True,bins=int(180/5), color = 'darkblue',hist_kws={'edgecolor':'black'},kde_kws={'linewidth':4})
 plt.title('HDDM Graph')
 plt.xlabel('Reaction Times')
+plt.ylabel('Frequency')
+plt.savefig("stone.png")
+
+# load the data from the data.csv file into a variable data_stone
+data_stone = hddm.load_csv('data.csv')
+
+# flips the signs back
+data_stone = hddm.utils.flip_errors(data_stone)
+
+# Instantiate model object passing it data_stone
+# This will tailor an individual hierarchical DDM around the dataset.
+m = hddm.HDDM(data_stone)
+
+# find a good starting point which helps with convergence
+starting_values = m.find_starting_values()
+
+# start drawing 7000 samples and discarding 5000 as burn-in
+m.sample(2000, burn=20)
+
+# Now we have an estimated model. We are going to print a summary stats table for each parameters' posterior.
+stats = m.gen_stats()
+stats[stats.index.isin(['a', 'a_std', 'a_subj.0', 'a_subj.1'])]
+
+# this is used to look at is the trace, the autocorrelation, and the marginal posterior
+plt.figure(2)
+m.plot_posteriors(['a', 't', 'v', 'a_std'])
+plt.savefig("posteriors.png")
+
+# now we want to get the ~2000 samples from the 1 chain for a,t,v (make it a pandas dataframe)
+df_posteriors = pd.DataFrame(m.get_traces())
+
+# put df_posteriors into a csv file
+a = open('posteriors.csv', 'wb')
 out = csv.writer(a, delimiter=',')
 df_posteriors.to_csv('posteriors.csv', header=True, index=True)
 
@@ -67,7 +175,7 @@ def recovery(possamps, truevals):  # Parameter recovery plots
     blue = np.array([0, 0, 1])
     orange = np.array([1, .3, 0])
     Colors = [teal, blue]
-
+    
     for v in range(0, nvars):
         # Compute percentiles
         bounds = stats.scoreatpercentile(alldata[v, :], (.5, 2.5, 97.5, 99.5))
@@ -89,7 +197,7 @@ def recovery(possamps, truevals):  # Parameter recovery plots
     tempx = np.linspace(np.min(truevals), np.max(truevals), num=100)
     recoverline = plt.plot(tempx, tempx)
     plt.setp(recoverline, linewidth=3, color=orange)
-
+    
 #now we want to make a 3 dimensional matrix to input to postamps posteriors.csv (for aU)
 posteriors_data = hddm.load_csv('posteriors.csv')
 a_postamps = pd.DataFrame([posteriors_data['a_subj.0'],posteriors_data['a_subj.1']])
